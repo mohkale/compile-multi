2351ee058edcab0543f5f114c860569ec05fee76
Mohsin Kaleem <mohkale@kisara.moe>
2023-08-27 22:51:54 +0100

    typo: ebmark -> embark

--- extensions/compile-multi-embark/compile-multi-embark.el
+++ extensions/compile-multi-embark/compile-multi-embark.el
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 
-;; This package defines a keymap and ebmark transformer helper to integrate
+;; This package defines a keymap and embark transformer helper to integrate
 ;; compile-multi into embark. This lets you set command actions that will
 ;; run on the underlying command shown in a `compile-multi' session.
 
